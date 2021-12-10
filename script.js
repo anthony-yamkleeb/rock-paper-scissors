e80117810c4c88a8cbad2476b4f42e056c29ca45
Anthony10799 <AnthonyYamkleeb10799@gmail.com>
2021-12-09 22:55:07 -0600

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 function computerPlay() {
     let moves = ["rock","paper","scissors"];
     let pickedMove = moves[Math.floor(Math.random()*moves.length)];
-    return pickedMove;
+    result = pickedMove;
+    return result;
+    
 }
 
 function playRound(playerSelection,compueterSelection) {
     playerSelection = playerSelection.toLowerCase();
     compueterSelection = compueterSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(compueterSelection);
     
     const rock = "rock";               // const are to hold the avalable
     const paper = "paper";             // moves that can be done.
-    const scissors = " scissors";
+    const scissors = "scissors";
+    
 
-    if (playerSelection === rock && compueterSelection === scissors) return "You win!";
-    if (playerSelection === scissors && compueterSelection === paper) return "You win!";
-    if (playerSelection === paper && compueterSelection === rock) return "You win!";
-    if (playerSelection === rock && compueterSelection === paper) return "You lose";
-    if (playerSelection === scissors && compueterSelection === rock) return "You lose";  
-    if (playerSelection === paper && compueterSelection === scissors) return "You lose";
-    if (playerSelection === compueterSelection) return "tie";   
+    if (playerSelection === rock && compueterSelection === scissors) return console.log("You win!");
+    if (playerSelection === rock && compueterSelection === paper) return console.log("You lose");
+    
+    if (playerSelection === paper && compueterSelection === rock) return console.log("You win!");
+    if (playerSelection === paper && compueterSelection === scissors) return console.log("You lose");
+
+    if (playerSelection === scissors && compueterSelection === paper) return console.log("You win!");
+    if (playerSelection === scissors && compueterSelection === rock) return console.log("You lose");
+
+    if (playerSelection === compueterSelection) return console.log("Tie");
 }
 
 const rock = document.createElement("button");
@@ -32,4 +40,13 @@ scissors.textContent = "scissors";
 buttons.appendChild(rock);
 buttons.appendChild(paper);
 buttons.appendChild(scissors);
+
+
+
+
+
+rock.addEventListener('click',() => {playRound("rock",computerPlay())});
+paper.addEventListener('click',() => {playRound("paper",computerPlay())});
+scissors.addEventListener('click',() => {playRound("scissors",computerPlay())});
+
 
